eebc2965a7feb6e889323715087f6c4ca1a63b9e
Odina <odinasayid@gmail.com>
2018-04-26 15:21:34 -0700

    created views and urls in game_app

--- apps/game_app/views.py
+++ apps/game_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 # the index function is called when root is visited
-def index(request):
+def render_game(request):
     if 'user' not in request.session:
         return redirect("/")
-    return render(request, 'game_app/index.html')
+    return render(request, 'game_app/game.html')
 def logout(request):
     request.session.flush()
     return redirect("/")
