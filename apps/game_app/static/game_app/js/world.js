49f48b08290d19963b020fa6432aac6d166f5ddc
Odina <odinasayid@gmail.com>
2018-04-27 17:25:47 -0700

    created file world.js, game.html and style.css and added images. World.js builds pacman world and positions players

---
+++ apps/game_app/static/game_app/js/world.js
@@ -0,0 +1,36 @@
+class world{
+    constructor(world_map){
+        this.player1 = '<img src = "/static/game_app/img/pacman1.gif">';
+        this.player2 = '<img src = "/static/game_app/img/pacman.gif">';
+        this.world_map = world_map;
+        this.player = {
+            'row': 3,
+            'col': 3,
+        }
+        this.player2Pos = {}
+        
+    }
+    drawWorld(){
+        var output = "";
+        var worldDict = {
+			0:'wall',
+			1: 'coin',
+			2: 'blank'
+        }
+        console.log(this.world_map);
+        //console.log(worldDict);
+        this.world_map[this.player['row']][this.player['col']] = 8;
+        for(var row = 0; row < this.world_map.length; row++){
+            output += "<div class = 'row'>"
+            for(var wall = 0; wall < this.world_map[row].length; wall++){
+                if(this.world_map[row][wall] == 8){ output += this.player1;}
+                else if(this.world_map[row][wall] == 9){ output += this.player2;}
+                //if(this.world_map[row][wall] == 0 || this.world_map[row][wall] == 1 || this.world_map[row][wall] == 2){output += "<div class = '" + worldDict[this.world_map[row][wall]] +"'></div>"}
+                else{output += "<div class = '" + worldDict[this.world_map[row][wall]] +"'></div>";}
+            }
+            output += "</div>"
+        }
+        $('#world').html(output)
+        //document.getElementById('world').innerHTML = output;
+    }
+}
